b7a683a3dd615a50ffdc67f7483f55c58d26ea48
AhsanInkiad <ahsaninkiadakib380082@gmail.com>
2025-01-02 21:44:00 +0600

    Add Footer.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "./Footer"
 import Header from "./Header"
 
 function App() {
@@ -5,6 +6,8 @@ function App() {
   return (
     <>
       <Header></Header>
+      
+      <Footer></Footer>
     </>
   )
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer className="bottom-0 py-6 md:py-8 text-black">
+            <div className="container mx-auto">
+                <p className="text-center text-xs text-[#6A6A6A] lg:text-sm">
+                    Copyright ©2024 | All rights reserved by Learn with Sumit
+                </p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
