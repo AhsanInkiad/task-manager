a7dd869a968387926e18791e69447258edad42ae
AhsanInkiad <ahsaninkiadakib380082@gmail.com>
2025-01-02 22:03:31 +0600

    Create Task Board UI.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import Footer from "./Footer"
 import Header from "./Header"
 import HeroSection from "./HeroSection"
+import TaskBoard from "./task/TaskBoard"
 
 function App() {
 
   return (
     <>
       <Header></Header>
-      <HeroSection></HeroSection>
+      <div className="flex flex-col justify-center items-center">
+        <HeroSection></HeroSection>
+        <TaskBoard></TaskBoard>
+      </div>
+
       <Footer></Footer>
     </>
   )
