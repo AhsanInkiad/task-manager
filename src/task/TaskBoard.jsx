fe4a9c01284e6916d56edc3002718f67f338764e
AhsanInkiad <ahsaninkiadakib380082@gmail.com>
2025-01-03 01:19:24 +0600

    Add favourite feature.

--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -42,6 +42,13 @@ function TaskBoard() {
         setTaskToUpdate(null)
     }
 
+    function  handleFavourite(taskId){
+        const taskIndex = tasks.findIndex(task=>task.id===taskId)
+        const newTasks = [...tasks];
+        newTasks[taskIndex].isFavourite = !newTasks[taskIndex].isFavourite
+        setTasks(newTasks)
+    }
+
     function handleDeleteTask(taskId){
         const tasksAfterDelete = tasks.filter(task=>task.id!==taskId);
         setTasks(tasksAfterDelete);
@@ -59,7 +66,7 @@ function TaskBoard() {
                 {/* Task List */}
                 <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
                     <TaskAction onDeleteAllClick={()=>setTasks([])} onAddClick={() => setShowAddModal(true)}></TaskAction>
-                    <TaskList onDelete={handleDeleteTask} tasks={tasks} onEdit={handleEditTask}></TaskList>
+                    <TaskList  handleFavourite={handleFavourite} onDelete={handleDeleteTask} tasks={tasks} onEdit={handleEditTask}></TaskList>
                 </div>
             </div>
         </section>
